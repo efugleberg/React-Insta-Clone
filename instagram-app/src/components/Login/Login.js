e6613fec5005956c540b6504547b4b8da5d7e856
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-05 16:46:44 -0500

    Day 3: login in page maybe working

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
-const Login = () => {
-    return (
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            loggedIn: false
+        }
+    }
 
-    )
+    login = e => {
+        e.preventDefault();
+        if (localStorage.getItem('loggedIn')) {
+            localStorage.removeItem('loggedIn');
+            this.setState({ 
+                loggedIn: true
+            })
+        } else {
+            localStorage.setItem('loggedIn', true);
+            this.setState({
+                loggedIn: true
+            })
+        }
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                <input
+                placeholder='name'
+                username=''
+                />
+                <input
+                placeholder='password'
+                password=''
+                />
+                
+                <button onSubmit={this.login}>Login</button>
+                </form>
+            </div>
+
+        );
+    }
 }
 
 export default Login;
