4aab62baf3bd1b20fd84cc91f7c9c128406500e7
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-06-05 15:40:39 -0500

    Day 3: original page is re-rendered

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
+
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)
 
 class App extends React.Component {
   constructor() {
@@ -13,34 +15,13 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    this.setState({
-      posts: dummyData
-    })
-  }
-
-  userSearch = e => {
-    const posts = this.state.posts.filter((user) => {
-      if (user.username.includes(e.target.value)) {
-        return user;
-        } return '';
-      })
-    this.setState({filteredPosts: posts})
-  }
-
   render() {
-    console.log(this.state.posts)
+    // console.log(this.state.posts)
   return (
     <div className="App">
-      <div className='search-bar'>
-        <SearchBar 
-          userSearch={this.userSearch} />
-      </div>
       <div className='post-container'>
-        <PostContainer 
-          posts={this.state.filteredPosts.length > 0 ?
-          this.state.filteredPosts :
-          this.state.posts} />
+        <ComponentFromWithAuthenticate
+        />
       </div>
     </div>
     );
